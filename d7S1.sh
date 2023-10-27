3ff473e468e7fe223040640a4729176a9a8d09c2
Lalit Adhikari <83487704+mutate9601@users.noreply.github.com>
2023-10-27 09:36:56 +0530

    Create d7S1.sh
    Day 7 script 1

---
+++ d7S1.sh
@@ -0,0 +1,7 @@
+#File Operations:
+#!/bin/bash/
+
+touch myfile.txt
+# A singel '>' will just put the same value in file again n again, where as '>>' will add/append value again n again
+echo "Hello, File!" > myfile.txt  
+cat myfile.txt
